d650b500da91e76971a98b4f9c6e30da045fe92f
karinaAlejandraOrtiz <67925023+karinaAlejandraOrtiz@users.noreply.github.com>
2020-07-06 16:10:21 -0300

    Add files via upload

---
+++ curso principiante java/app.js
@@ -0,0 +1,36 @@
+/*
+//suma de dos variables
+var resultado;
+var numero1= 5;
+var numero2= 17;
+
+resultado= numero1 + numero2;
+
+alert("el resultado es: " + resultado);
+*/
+
+//pedirle dos nomeros al usuario y sumarlos. Mostrarlos por alert
+
+
+var a;
+var b; 
+
+/*a= prompt ("ingrese un numero: ");
+a= parseInt(a);*/
+
+a= parseInt(prompt ("ingrese un numero: "));
+
+b= prompt ("ingrese otro numero: ");
+b= parseInt(b);
+resultado = parseInt(a+b);
+
+alert (resultado);
+
+
+
+
+
+
+
+
+
---
+++ curso principiante java/promedio.js
@@ -0,0 +1,9 @@
+var operaciones;
+
+var mensaje1= prompt("ingrese un numero: ");
+
+var mensaje2= prompt("ingrese la operacion a realizar: ");
+
+var mensaje3= prompt("ingrese otro numero: ")
+
+
