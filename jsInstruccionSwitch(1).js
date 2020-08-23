2e4e2bfeac57198ed87f1e7c73d82973a4a7c4e6
karinaAlejandraOrtiz <67925023+karinaAlejandraOrtiz@users.noreply.github.com>
2020-08-23 18:12:39 -0300

    Add files via upload

---
+++ jsInstruccionSwitch(1).js
@@ -0,0 +1,10 @@
+function mostrar()
+{/*al seleccionar un mes informar.
+    si es Enero: "que comiences bien el año!!!."
+    si es Marzo: "a clases!!!."
+    si es Julio: "se vienen las vacaciones!!!."
+    si es Diciembre: "Felices fiesta!!!."*/
+//tomo la edad  
+
+
+}//FIN DE LA FUNCIÓN
