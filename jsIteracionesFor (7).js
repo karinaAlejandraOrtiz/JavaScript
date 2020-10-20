89d77e87964d96aa81052694777906dea6d8a346
karinaAlejandraOrtiz <67925023+karinaAlejandraOrtiz@users.noreply.github.com>
2020-10-20 08:09:27 -0300

    Add files via upload

---
+++ jsIteracionesFor (7).js
@@ -0,0 +1,17 @@
+  
+function mostrar()
+{
+var num= prompt("ingrese un numero")
+var cont=0
+
+for (var i=0;i<=num;i++)
+{
+    if ((num%i)==0)
+    {
+        cont++
+        alert("es divisor: "+i)
+    }
+}
+alert("hay "+cont+" numeros divisores");
+
+}//FIN DE LA FUNCIÓN
